1be58293edf0d46e5d7e343421b95c9a9dc603a6
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2014-07-08 23:05:33 +0900

    added actions.
    Create of templates.
    List of templates.

--- app.rb
+++ app.rb
@@ -2,9 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader' if development?
-
+require 'sinatra/activerecord'
 DOWNLOAD_FILE_NAME = 'download.xlsx'
 CONTENT_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
 
 set :root, File.dirname(__FILE__)
 set :port, 3000
@@ -14,14 +15,26 @@ get '/' do
   "ruby version #{RUBY_VERSION}"
 end
 
+class Template < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :data, presence: true
+end
+
 # Upload Template file
 post '/templates/create' do
-
+  begin
+    Template.create!(title: params['title'], data: params['data'])
+  rescue ActiveRecord::RecordInvalid => e
+    status 500
+    {message: e.message}.to_json
+  end
+  status 200
+  {message: 'OK'}.to_json
 end
 
 # list of Template files
 get '/templates' do
-
+  Template.pluck(:title).to_json
 end
 
 # create Excel file
